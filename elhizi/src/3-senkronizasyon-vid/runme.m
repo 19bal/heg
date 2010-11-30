fec78b4a698bc78de10c1c8984db7d4e909fc34a
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-11-30 08:50:45 +0200

    extract marker->compute alpha: basladik

--- elhizi/src/3-senkronizasyon-vid/runme.m
+++ elhizi/src/3-senkronizasyon-vid/runme.m
@@ -8,8 +8,8 @@ addpath(LIB_PATH,'-end');                                                 %
 
 dbg = false;
 
-dbnm = pathos('_db/orj/');
-dbnm_sel = pathos('_db/sel/');
+dbnm = pathos('_db/orj/');          % bu dizine db resimlerini koy!
+dbnm_sel = pathos('_db/sel/');      mkdir(dbnm_sel);
 
 DIR = dir(strcat(dbnm, '*.png'));
 
@@ -19,3 +19,17 @@ if length(dir(dbnm_sel)) == 2
     sel_frame(DIR, dbnm, dbnm_sel, ts, te, dbg);
 end
 
+DIR = dir(strcat(dbnm_sel, '*.png'));
+sz  = length(DIR);
+for f=1:sz
+    imgnm = DIR(f).name;    
+    fr = imread(strcat(dbnm_sel, imgnm));
+    
+    % extract marker
+    % compute alpha
+    
+    if dbg
+        figure(1);  imshow(fr)
+        drawnow;
+    end
+end
