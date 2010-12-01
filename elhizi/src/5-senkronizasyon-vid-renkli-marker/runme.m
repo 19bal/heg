442fd2b409a1e6d76846f2d9d33551e1dd68892a
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-12-01 14:47:04 +0200

    extract_marker a aktarmadan oncesi

--- elhizi/src/5-senkronizasyon-vid-renkli-marker/runme.m
+++ elhizi/src/5-senkronizasyon-vid-renkli-marker/runme.m
@@ -5,28 +5,27 @@ clear all;  close all;  clc;
 LIB_PATH = sprintf('..%s..%s..%slib%s', filesep,filesep,filesep,filesep);                         %
 addpath(LIB_PATH,'-end');                                                 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sc = 0.25;
+T = 50;
+
+minDist = 30;
+maxDist = 325;
 
 dbg = true;
 
-dbnm = pathos('_db/orj/');          % bu dizine db resimlerini koy!
-dbnm_sel = pathos('_db/sel/');      mkdir(dbnm_sel);
-dbnm_bw = pathos('_db/bw/');        mkdir(dbnm_bw);
-
-if length(dir(dbnm_sel)) == 2
-    DIR = dir(strcat(dbnm, '*.png'));
-    [ts, te] = extract_extreme_time(DIR, dbnm, dbg);
-    sel_frame(DIR, dbnm, dbnm_sel, ts, te, dbg);
-end
+dbnm = pathos('_db/orj/');              % bu dizine db resimlerini koy!
+dbnm_bw = pathos('_db/bw/');            mkdir(dbnm_bw);
+dbnm_marker = pathos('_db/marker/');    mkdir(dbnm_marker);
 
 if length(dir(dbnm_bw)) == 2
-    fr2bw(dbnm_sel, dbnm_bw, 0.25, 50, dbg);
+    fr2bw(dbnm, dbnm_bw, sc, T, dbg);
 end
 
 DIR = dir(strcat(dbnm, '*.png'));
 DIR_bw = dir(strcat(dbnm_bw, '*.png'));
 sz  = length(DIR_bw);
 
-fi = 5:(sz-5);
+fi = 1:sz;
 for f=fi
     if dbg,
         fprintf('%04d/%04d. frame isleniyor...\n', f,sz);
@@ -41,12 +40,62 @@ for f=fi
     bw3 = cat(3, bw2,bw2,bw2);
     fr_m = fr .* uint8(bw3);
 
+    % % fr_m->bw
+    b = (fr_m > 100);
+    bb = max(max(b(:,:,1), b(:,:,2)), b(:,:,3));
+    
+    % % temizlik
+    te = imerode(bb, strel('square', 8));
+    td = imdilate(te, strel('square', 8));
+    
+    % % markerlari sec
+    L = bwlabel(td);
+    s = regionprops(L, {'centroid', 'eccentricity', 'area'});
+
+    % % % ilk
+    centroid = cat(1, s.Centroid);
+    eccentricity = cat(1, s.Eccentricity);
+    area = cat(1, s.Area);
+    
+    % % % centroid: birbirinden cok uzak olmasin
+    resim_merkezi = size(td)/2;
+    ed = euclid_distance(centroid, resim_merkezi)';
+    idx = find((ed > minDist) & (ed < maxDist));
+    
+    % % % eccentricity: 0 a yakin olanlar
+    ii = find((eccentricity(idx) < 0.70) & ...
+               (area(idx) > 350) & (area(idx) < 650) );
+    tmp_ecc = eccentricity(ii);
+    [t, iii] = sort(tmp_ecc);
+    idx = idx(ii(iii));
+           
+    % % % area: benzer alanlilar
+    if length(idx) > 3        
+        tmp_area = area(idx);
+        [sa, ids] = sort(tmp_area);
+        fark = diff(sa);
+        [t, ida] = sort(fark);
+        ida = ida + 1;
+
+        ii = unique([ida(1) ida(1)-1 ida(2) ida(2)-1]);
+        idx = idx(ids(ii));
+    end
+    
+    if length(idx) < 3
+        error('3 den az marker bulundu.');
+    else
+        idx = idx(1:3);
+    end
+    
+    bw_m = ismember(L, idx);
+    imwrite(bw_m, strcat(dbnm_marker, imgnm));
+    
     % compute alpha
 
     if dbg
         figure(1);  
-        subplot(121);   imshow(fr)
-        subplot(122);   imshow(fr_m)
+        subplot(121);   imshow(td)
+        subplot(122);   imshow(bw_m)
         drawnow;
     end
 end
