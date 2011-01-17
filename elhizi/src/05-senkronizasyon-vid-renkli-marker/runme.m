fcb5b5d1f9b60d420f02bc888d213c10a47cf17f
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2011-01-17 14:19:18 +0200

    null

--- elhizi/src/05-senkronizasyon-vid-renkli-marker/runme.m
+++ elhizi/src/05-senkronizasyon-vid-renkli-marker/runme.m
@@ -10,7 +10,7 @@ T = 50;
 
 dbg = true;
 
-dbnm = pathos('_db\orj\');              if ~exist(dbnm), error('Bu dizini olustur ve test resimlerini bu dizine koy!'); end
+dbnm = pathos('_db\orj\');              if ~exist(dbnm), error(sprintf('"%s" dizinini olustur ve test resimlerini bu dizine koy!', dbnm)); end
 dbnm_bw = pathos('_db\bw\');            mkdir(dbnm_bw);
 dbnm_marker = pathos('_db\marker\');    mkdir(dbnm_marker);
 
@@ -30,25 +30,25 @@ for f=1:650%sz
     if dbg,
         fprintf('%04d/%04d. frame isleniyor...\n', f,sz);
     end
-    
-    imgnm = DIR(f).name;    
+
+    imgnm = DIR(f).name;
     fr = imadjustRGB(imread(strcat(dbnm, imgnm)));
-    bw_m = imread(strcat(dbnm_marker, imgnm));    
-    
+    bw_m = imread(strcat(dbnm_marker, imgnm));
+
     hand = marker_assign(fr, bw_m, dbg);
     alpha(f) = compute_alpha(hand);
-    
+
     if dbg
-        figure(1);  
+        figure(1);
         subplot(121);   imshow(fr)
         subplot(122);   imshow(1 - bw_m)
         hold on;
         plot(hand.index(1), hand.index(2), '*g')
         plot(hand.palm(1),  hand.palm(2),  '*r')
         plot(hand.thumb(1), hand.thumb(2), '*y')
-        hold off;        
+        hold off;
         drawnow;
-    end    
+    end
 end
 
 if dbg
